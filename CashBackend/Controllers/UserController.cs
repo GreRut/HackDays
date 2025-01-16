89ad3cdd457e7c39039f658b691cb5cf8d0bc6c6
GreRut <ruta.grzegorz@gmail.com>
2025-01-16 23:41:18 +0100

    fix: adjust request and response handling in POST user

--- CashBackend/Controllers/UserController.cs
+++ CashBackend/Controllers/UserController.cs
@@ -18,13 +18,41 @@ namespace CashBackend.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<UserResponse>>> GetUsers()
         {
-            return await _context.Users.Select(it => new User
+            return await _context.Users.Select(it => new UserResponse
             {
-                Id = it.Id,
                 Name = it.Name,
             }).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserResponse>> GetUser(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+            var response = new UserResponse
+            {
+                Name = users.Name,
+            };
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(UserRequest request)
+        {
+            var userRequest = new User()
+            {
+                Name = request.Name,
+            };
+
+            _context.Users.Add(userRequest);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetUser), new { id = userRequest.Id }, userRequest);
         }
     }
 }
