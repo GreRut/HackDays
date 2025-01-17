fc56f7ffa91e1de51bfd212f243ee68cf6eaad48
GreRut <ruta.grzegorz@gmail.com>
2025-01-17 12:05:43 +0100

    feat(GetUserBalance): implement greedy algorithm to calculate user expenses

--- CashBackend/Controllers/UserController.cs
+++ CashBackend/Controllers/UserController.cs
@@ -29,15 +29,15 @@ namespace CashBackend.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<UserResponse>> GetUser(int id)
         {
-            var users = await _context.Users.FindAsync(id);
+            var user = await _context.Users.FindAsync(id);
 
-            if (users == null)
+            if (user == null)
             {
                 return NotFound();
             }
             var response = new UserResponse
             {
-                Name = users.Name,
+                Name = user.Name,
             };
             return Ok(response);
         }
@@ -53,6 +53,54 @@ namespace CashBackend.Controllers
             _context.Users.Add(userRequest);
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetUser), new { id = userRequest.Id }, userRequest);
+        }
+
+        [HttpGet("{id}/balance")]
+        public async Task<ActionResult<string>> GetUserBalance(int id)
+        {
+            var pick = await _context.Users.FindAsync(id);
+
+            if (pick == null)
+            {
+                return NotFound($"User with ID {id} not found.");
+            }
+
+            var user = new UserResponse
+            {
+                Name = pick.Name,
+            };
+
+            var _users = await _context.Users.Select(it => new UserResponse
+            {
+                Name = it.Name,
+            }).ToListAsync();
+
+            var _items = await _context.Items.Select(it => new ItemPriceResponse
+            {
+                Id = it.Id,
+                Price = it.Price,
+                UserId = it.UserId
+            }).ToListAsync();
+
+            int totalExpenses = _items.Sum(i => i.Price);
+            int numberOfUsers = _users.Count;
+
+            int fairShare = totalExpenses / numberOfUsers;
+
+            Dictionary<int, int> netBalances = new Dictionary<int, int>(); 
+            foreach (var u in _users)
+            {
+                int userExpenses = _items.Where(i => i.UserId == u.Id).Sum(i => i.Price);
+                netBalances[u.Id] = userExpenses - fairShare;
+            }
+            int userBalance = netBalances[id];
+
+            if (userBalance == 0)
+                return Ok($"User {user.Name} is settled with the group.");
+            else if (userBalance > 0)
+                return Ok($"User {user.Name} is owed {userBalance} by the group.");
+            else
+                return Ok($"User {user.Name} owes {-userBalance} to the group.");
         }
     }
 }
--- CashBackend/Models/ItemPriceResponse.cs
+++ CashBackend/Models/ItemPriceResponse.cs
@@ -1,11 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace CashBackend.Models
 {
-    public class Item
+    public class ItemPriceResponse
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
         public required int Price { get; set; }
         public required int UserId { get; set; }
-        public virtual User? User {get; set;}
     }
 }
